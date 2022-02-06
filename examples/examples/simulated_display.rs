6a77c4b2c4b06dd8c8d0c856b7d567a07e8dd3b1
Ferris Tseng <ferristseng@fastmail.fm>
2022-02-06 16:47:36 -0500

    formatting, and updating crate names

--- examples/examples/simulated_display.rs
+++ examples/examples/simulated_display.rs
@@ -1,17 +1,33 @@
 use embedded_graphics::{
     mono_font::MonoTextStyle, pixelcolor::BinaryColor, prelude::*, text::Text,
 };
-use embedded_graphics_cjk_sarasa_gothic::SARASA_MONO_SLAB_SC_LIGHT_24;
-use embedded_graphics_cjk_zpix::ZPIX_12;
+use embedded_graphics_cjk_font_sarasa_gothic::SARASA_MONO_SC_LIGHT_24;
+use embedded_graphics_cjk_font_zpix::{ZPIX_12, ZPIX_24};
 use embedded_graphics_simulator::{
     BinaryColorTheme, OutputSettingsBuilder, SimulatorDisplay, Window,
 };
 
 fn main() -> Result<(), core::convert::Infallible> {
-    let mut display = SimulatorDisplay::<BinaryColor>::new(Size::new(256, 64));
+    let mut display = SimulatorDisplay::<BinaryColor>::new(Size::new(256, 80));
 
-    Text::new("大沼澤地國家公園", Point::new(0, 0), MonoTextStyle::new(&SARASA_MONO_SLAB_SC_LIGHT_24, BinaryColor::On)).draw(&mut display)?;
-    Text::new("大沼澤地國家公園", Point::new(0, 24), MonoTextStyle::new(&ZPIX_12, BinaryColor::On)).draw(&mut display)?;
+    Text::new(
+        "大沼澤地國家公園",
+        Point::new(0, 0),
+        MonoTextStyle::new(&SARASA_MONO_SC_LIGHT_24, BinaryColor::On),
+    )
+    .draw(&mut display)?;
+    Text::new(
+        "大沼澤地國家公園",
+        Point::new(0, 24),
+        MonoTextStyle::new(&ZPIX_12, BinaryColor::On),
+    )
+    .draw(&mut display)?;
+    Text::new(
+        "新年快乐",
+        Point::new(0, 48),
+        MonoTextStyle::new(&ZPIX_24, BinaryColor::On),
+    )
+    .draw(&mut display)?;
 
     let output_settings = OutputSettingsBuilder::new()
         .theme(BinaryColorTheme::OledBlue)
