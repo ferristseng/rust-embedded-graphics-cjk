fe0d3b7fe572e4cc49959f486da8c1b654c53e4c
Ferris Tseng <ferristseng@fastmail.fm>
2022-04-13 00:55:17 -0400

    update example to showcase different fonts

--- examples/examples/simulated_display.rs
+++ examples/examples/simulated_display.rs
@@ -1,43 +1,56 @@
 use embedded_graphics::{
-    mono_font::MonoTextStyle,
-    pixelcolor::BinaryColor,
-    prelude::*,
-    text::{Alignment, Text},
+    mono_font::MonoTextStyle, pixelcolor::BinaryColor, prelude::*, text::Text,
 };
+use embedded_graphics_cjk_font_fusion_pixel::{FUSION_PIXEL_12, FUSION_PIXEL_24};
 use embedded_graphics_cjk_font_noto::NOTO_SANS_MONO_CJK_SC_REGULAR_36;
 use embedded_graphics_cjk_font_sarasa_gothic::SARASA_MONO_SC_LIGHT_36;
-use embedded_graphics_cjk_font_zpix::ZPIX_24;
-use embedded_graphics_simulator::{
-    BinaryColorTheme, OutputSettingsBuilder, SimulatorDisplay, Window,
-};
+use embedded_graphics_cjk_font_zpix::{ZPIX_12, ZPIX_24};
+use embedded_graphics_simulator::{OutputSettingsBuilder, SimulatorDisplay, Window};
+
+const TEXT: &'static str = "大沼澤地國家公園";
 
 fn main() -> Result<(), core::convert::Infallible> {
     let mut display = SimulatorDisplay::<BinaryColor>::new(Size::new(512, 128));
 
-    Text::new(
-        "大沼澤地國家公園",
-        Point::new(0, 0),
-        MonoTextStyle::new(&SARASA_MONO_SC_LIGHT_36, BinaryColor::On),
-    )
-    .draw(&mut display)?;
-    Text::new(
-        "大沼澤地國家公園",
-        Point::new(0, 36),
-        MonoTextStyle::new(&NOTO_SANS_MONO_CJK_SC_REGULAR_36, BinaryColor::On),
-    )
-    .draw(&mut display)?;
-    Text::with_alignment(
-        "新年快乐",
-        Point::new(0, 72),
-        MonoTextStyle::new(&ZPIX_24, BinaryColor::On),
-        Alignment::Left,
-    )
-    .draw(&mut display)?;
+    let width_multiplier = TEXT.chars().count() as i32 + 1;
+    let lines_to_draw = &[
+        Text::new(
+            TEXT,
+            Point::new(0, 0),
+            MonoTextStyle::new(&SARASA_MONO_SC_LIGHT_36, BinaryColor::On),
+        ),
+        Text::new(
+            TEXT,
+            Point::new(0, 36),
+            MonoTextStyle::new(&NOTO_SANS_MONO_CJK_SC_REGULAR_36, BinaryColor::On),
+        ),
+        Text::new(
+            TEXT,
+            Point::new(0, 72),
+            MonoTextStyle::new(&FUSION_PIXEL_24, BinaryColor::On),
+        ),
+        Text::new(
+            TEXT,
+            Point::new(24 * width_multiplier, 72),
+            MonoTextStyle::new(&ZPIX_24, BinaryColor::On),
+        ),
+        Text::new(
+            TEXT,
+            Point::new(0, 96),
+            MonoTextStyle::new(&FUSION_PIXEL_12, BinaryColor::On),
+        ),
+        Text::new(
+            TEXT,
+            Point::new(12 * width_multiplier, 96),
+            MonoTextStyle::new(&ZPIX_12, BinaryColor::On),
+        ),
+    ];
 
-    let output_settings = OutputSettingsBuilder::new()
-        .theme(BinaryColorTheme::OledBlue)
-        .scale(1)
-        .build();
+    for line in lines_to_draw {
+        line.draw(&mut display)?;
+    }
+
+    let output_settings = OutputSettingsBuilder::new().scale(1).build();
     Window::new("Test", &output_settings).show_static(&display);
 
     Ok(())
